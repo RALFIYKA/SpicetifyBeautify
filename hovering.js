3851c59eec486aa1f488837d7e89ab35a47038a6
MR-d3R <47676479+MR-d3R@users.noreply.github.com>
2021-01-29 23:31:19 +0300

    It deosn't actally work

--- hovering.js
+++ hovering.js
@@ -1,10 +1,32 @@
-var a = document.querySelectorAll('.btn');
+// window.onload = function () {
 
-//перебираем все найденные элементы и вешаем на них события
-[].forEach.call(a, function (el) {
-    //вешаем событие
-    el.onclick = function (e) {
-        //производим действия
-        alert('Hello');
-    }
-});
+//     //получаем идентификатор элемента
+//     var a = document.getElementById('switch');
+
+//     //вешаем на него событие
+//     a.onclick = function () {
+//         //производим какие-то действия
+//         if (this.innerHTML == 'On') this.innerHTML = 'Off';
+//         else this.innerHTML = 'On';
+//         //предотвращаем переход по ссылке href
+//         return false;
+//     }
+// }
+
+// var a = document.querySelectorAll('.btn');
+
+// //перебираем все найденные элементы и вешаем на них события
+// [].forEach.call(a, function (el) {
+//     //вешаем событие
+//     el.onclick = function (e) {
+//         //производим действия
+//         var runnableScript = exec('C:\\Users\\Someone\\Documents\\testfile.bat)',
+//             (error, stdout, stderr) => {
+//                 console.log(stdout);
+//                 console.log(stderr);
+//                 if (error !== null) {
+//                     console.log(`exec error: ${error}`);
+//                 }
+//             });
+//     }
+// });

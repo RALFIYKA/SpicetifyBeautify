64d642b69e8791fc4e82358a6a6f53ccbc80bcde
MR-d3R <47676479+MR-d3R@users.noreply.github.com>
2021-02-01 22:07:01 +0300

    shell?

--- hovering.js
+++ hovering.js
@@ -1,3 +1,51 @@
+const { shell } = require('electron');
+const remote = require('electron').remote;
+// const app = remote.app;
+const fileButton = document.getElementById('fileButton');
+
+(function ($) {
+    $.fn.toJson = function (options) {
+        let settings = $.extend({
+            stringify: false
+        }, options);
+        let json = {};
+        $.each(this.serializeArray(), function () {
+            let name = this.name.replace(/[^\w\s]/gi, '');
+            if (name in json) {
+                if (!json[name].push)
+                    json[name] = [json[name]];
+                json[name].push(this.value || '');
+            } else
+                json[name] = this.value || '';
+        });
+        if (settings.stringify)
+            return JSON.stringify(json);
+        else
+            return json;
+    };
+})(jQuery);
+
+
+$('#fileButton').click(function () {
+    if (shell.openItem(app.getPath("downloads") + "ege26.doc")) {
+        ipcRenderer.send('app-quit', '');
+    }
+});
+
+// let myFile = null
+
+
+// fileButton.ondragover = fileButton.ondragend = fileButton.ondragleave = () => {
+//     return false;
+// };
+
+// fileButton.ondrop = (event) => {
+//     console.log(event.dataTransfer.files)
+//     myFile = event.dataTransfer.files[0].path;
+//     shell.openItem(myFile)
+//     return false;
+// }
+
 // window.onload = function () {
 
 //     //получаем идентификатор элемента

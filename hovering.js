4391bb4b9282fea4e9adb3ad3df66bf570a4ab01
MR-d3R <47676479+MR-d3R@users.noreply.github.com>
2021-02-02 21:31:46 +0300

    JS!

--- hovering.js
+++ hovering.js
@@ -1,80 +1,218 @@
 const { shell } = require('electron');
-const remote = require('electron').remote;
-// const app = remote.app;
-const fileButton = document.getElementById('fileButton');
-
-(function ($) {
-    $.fn.toJson = function (options) {
-        let settings = $.extend({
-            stringify: false
-        }, options);
-        let json = {};
-        $.each(this.serializeArray(), function () {
-            let name = this.name.replace(/[^\w\s]/gi, '');
-            if (name in json) {
-                if (!json[name].push)
-                    json[name] = [json[name]];
-                json[name].push(this.value || '');
-            } else
-                json[name] = this.value || '';
-        });
-        if (settings.stringify)
-            return JSON.stringify(json);
-        else
-            return json;
-    };
-})(jQuery);
 
 
-$('#fileButton').click(function () {
-    if (shell.openItem(app.getPath("downloads") + "ege26.doc")) {
-        ipcRenderer.send('app-quit', '');
-    }
-});
+fileButton1.onclick = function () {
+    shell.openExternal("C:/Windows/system32/cmd.exe");
+};
 
-// let myFile = null
+fileButton2.onclick = function () {
+    shell.openExternal("C:/Windows/system32/cmd.exe");
+};
 
+fileButton3.onclick = function () {
+    shell.openExternal("C:/Windows/system32/cmd.exe");
+};
 
-// fileButton.ondragover = fileButton.ondragend = fileButton.ondragleave = () => {
-//     return false;
+fileButton4.onclick = function () {
+    shell.openExternal("C:/Windows/system32/cmd.exe");
+};
+
+// fileButton5.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
 // };
 
-// fileButton.ondrop = (event) => {
-//     console.log(event.dataTransfer.files)
-//     myFile = event.dataTransfer.files[0].path;
-//     shell.openItem(myFile)
-//     return false;
-// }
+// fileButton6.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
 
-// window.onload = function () {
+// fileButton7.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
 
-//     //получаем идентификатор элемента
-//     var a = document.getElementById('switch');
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
 
-//     //вешаем на него событие
-//     a.onclick = function () {
-//         //производим какие-то действия
-//         if (this.innerHTML == 'On') this.innerHTML = 'Off';
-//         else this.innerHTML = 'On';
-//         //предотвращаем переход по ссылке href
-//         return false;
-//     }
-// }
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
 
-// var a = document.querySelectorAll('.btn');
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
 
-// //перебираем все найденные элементы и вешаем на них события
-// [].forEach.call(a, function (el) {
-//     //вешаем событие
-//     el.onclick = function (e) {
-//         //производим действия
-//         var runnableScript = exec('C:\\Users\\Someone\\Documents\\testfile.bat)',
-//             (error, stdout, stderr) => {
-//                 console.log(stdout);
-//                 console.log(stderr);
-//                 if (error !== null) {
-//                     console.log(`exec error: ${error}`);
-//                 }
-//             });
-//     }
-// });
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };
+
+// fileButton3.onclick = function () {
+//     shell.openExternal("C:/Windows/system32/cmd.exe");
+// };

a0b3f586dc9b9b2f2409115bf1c70392d2746191
MR-d3R <47676479+MR-d3R@users.noreply.github.com>
2021-02-16 16:07:37 +0300

    minor fixes

--- hovering.js
+++ hovering.js
@@ -1,8 +1,15 @@
 const { shell } = require('electron');
 const os = require('os');
 const username = os.userInfo().username;
+window.$ = window.jQuery = require('jquery');
 // On installtion (after spicetify) copy config ini  and use it like backup
 // Themes are actually ZIP from github
+
+function sendForm(form) {
+    let fullOutput = $(form).serialize();
+    let themeName = fullOutput.slice(fullOutput.search('=') + 1);
+    return fileOpen(themeName);
+}
 
 function fileOpen(themeName) {
     shell.openExternal("C:/tools/Spicetify-themes-app/res/" + themeName + ".png")

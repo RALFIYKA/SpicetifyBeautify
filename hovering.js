1bd0a2d2ee5baf77a3ff6b50d0f47070be00ec62
MR-d3R <47676479+MR-d3R@users.noreply.github.com>
2021-03-04 22:29:39 +0300

    titlebar updates

--- hovering.js
+++ hovering.js
@@ -2,6 +2,7 @@ const { shell } = require('electron');
 const os = require('os');
 const username = os.userInfo().username;
 window.$ = window.jQuery = require('jquery');
+
 // On installtion (after spicetify) copy config ini  and use it like backup
 // Themes are actually ZIP from github
 
@@ -15,30 +16,9 @@ function fileOpen(themeName) {
     shell.openExternal(process.cwd() + "/scripts/" + themeName + ".bat");
 };
 
-$("#menu a").click(function () {
-    var link = $(this).attr('href');
-    var showIt = $(link);
-    var hideIt = $(".showing.current");
-
-    hideIt.fadeOut(100, function () {
-        hideIt.removeClass("current");
-        showIt.addClass("current");
-        showIt.fadeIn(100);
-    });
+$(document).mousedown(function (e) {
+    if (e.which === 2) {
+        alert("Ples don' toch");
+        return false; // Or e.preventDefault()
+    }
 });
-
-// let currentPage = 1;
-// let nextPage = 1;
-// function sendPage(page) {
-//     let fullOutpu = $(page).serialize();
-//     nextPage = fullOutput.slice(fullOutput.search('=') + 1); // int??
-//     // $('hidden="true"').hide();
-//     // $(document).on('click', '.item-title', function (e) {
-//     //     e.preventDefault();
-//     //     $(this).next('.item-content').toggle();
-//     // });
-//     var dynamicEl = document.getElementById('themePage' + currentPage);
-//     dynamicEl.id = 'themePage' + nextPage;
-//     currentPage = nextPage;
-//     return false;
-// };

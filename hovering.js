cb4e1234f986efc90366fef80856f7e4f7119362
MR-d3R <47676479+MR-d3R@users.noreply.github.com>
2021-03-04 19:41:46 +0300

    scroll

--- hovering.js
+++ hovering.js
@@ -15,6 +15,18 @@ function fileOpen(themeName) {
     shell.openExternal(process.cwd() + "/scripts/" + themeName + ".bat");
 };
 
+$("#menu a").click(function () {
+    var link = $(this).attr('href');
+    var showIt = $(link);
+    var hideIt = $(".showing.current");
+
+    hideIt.fadeOut(100, function () {
+        hideIt.removeClass("current");
+        showIt.addClass("current");
+        showIt.fadeIn(100);
+    });
+});
+
 // let currentPage = 1;
 // let nextPage = 1;
 // function sendPage(page) {

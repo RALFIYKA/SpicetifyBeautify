ea82e3bf4aa0c8a59c64097da1f130beae9a51bc
RALFIYKA <72037566+RALFIYKA@users.noreply.github.com>
2021-03-15 21:27:29 +0300

    prettier cmd and fixes

--- scripts/Arc-Dark.bat
+++ scripts/Arc-Dark.bat
@@ -1,21 +1,36 @@
 @echo OFF
 color 0B 
-mode con:cols=100 lines=15
+mode con:cols=49 lines=25
 echo.
 echo.
-echo.
-@echo   ллллл    лллллл       ллллл       лл     лллллл   лллллл 
-@echo  лл    лл  лл   лл    лл          лл  лл   лл   лл  лл   лл
-@echo  ллл       лл   лл   лл          лл    лл  лл   лл  лл   лл
-@echo   ллл      ллллл     лл          лллллллл  ллллл    ллллл
-@echo    ллл     лл        лл          лл    лл  лл       лл
-@echo       лл   лл         лл         лл    лл  лл       лл
-@echo  ллллл     лл           ллллл    лл    лл  лл       лл
+echo.                        
+@echo                 .-:/++oooo++/:-.`                
+@echo             .:+oooooooooooooooooo+:.             
+@echo         `:oooooooooooooooooooooooooo/.          
+@echo        `/oooooooooooooooooooooooooooooo+.        
+@echo       :oooooooooooooooooooooooooooooooooo/       
+@echo     `+oooooooooooooooooooooooooooooooooooo+`     
+@echo    `+oooo+:--.``          ``.--:/+oooooooooo`    
+@echo    /ooooo                          .-/oooooo+    
+@echo   .oooooo/--:/++oooooooooo++/::-`      .ooooo-   
+@echo   /oooooooooooo+++/////++ooooooooo+:-``-ooooo+   
+@echo   oooooooo+-.`             `.-:/+ooooooooooooo   
+@echo   oooooooo/```.-::::::::--.`     `.:+ooooooooo   
+@echo   +ooooooooooooooooooooooooooo/:.`  `oooooooo+   
+@echo   -ooooooooo+/::--.....--::/+ooooo+/+oooooooo:   
+@echo    +ooooooo-  ``...----..`    .-/oooooooooooo    
+@echo    `oooooooooooooooooooooooo/:.`  +ooooooooo.    
+@echo     `ooooooooooooooooooooooooooo++ooooooooo.     
+@echo      `/oooooooooooooooooooooooooooooooooo+`      
+@echo        .+oooooooooooooooooooooooooooooo+-        
+@echo         ./oooooooooooooooooooooooooo+-          
+@echo            `-/oooooooooooooooooooo+-`            
+@echo               .-:/+oooooooo+/:-.                                                            
 @echo.
 @echo off   
 @powershell rm $(spicetify -c) >nul
-echo y|spicetify restore backup  >nul
+spicetify backup aplly >nul
+echo y|spicetify restore backup >nul
 spicetify apply >nul
 spicetify config current_theme Arc-Dark >nul
 spicetify apply >nul
-pause
--- scripts/Dobby.bat
+++ scripts/Dobby.bat
@@ -1,4 +1,35 @@
+@echo OFF
+color 0B 
+mode con:cols=49 lines=25
+echo.
+echo.
+echo.                        
+@echo                 .-:/++oooo++/:-.`                
+@echo             .:+oooooooooooooooooo+:.             
+@echo         `:oooooooooooooooooooooooooo/.          
+@echo        `/oooooooooooooooooooooooooooooo+.        
+@echo       :oooooooooooooooooooooooooooooooooo/       
+@echo     `+oooooooooooooooooooooooooooooooooooo+`     
+@echo    `+oooo+:--.``          ``.--:/+oooooooooo`    
+@echo    /ooooo                          .-/oooooo+    
+@echo   .oooooo/--:/++oooooooooo++/::-`      .ooooo-   
+@echo   /oooooooooooo+++/////++ooooooooo+:-``-ooooo+   
+@echo   oooooooo+-.`             `.-:/+ooooooooooooo   
+@echo   oooooooo/```.-::::::::--.`     `.:+ooooooooo   
+@echo   +ooooooooooooooooooooooooooo/:.`  `oooooooo+   
+@echo   -ooooooooo+/::--.....--::/+ooooo+/+oooooooo:   
+@echo    +ooooooo-  ``...----..`    .-/oooooooooooo    
+@echo    `oooooooooooooooooooooooo/:.`  +ooooooooo.    
+@echo     `ooooooooooooooooooooooooooo++ooooooooo.     
+@echo      `/oooooooooooooooooooooooooooooooooo+`      
+@echo        .+oooooooooooooooooooooooooooooo+-        
+@echo         ./oooooooooooooooooooooooooo+-          
+@echo            `-/oooooooooooooooooooo+-`            
+@echo               .-:/+oooooooo+/:-.                                                            
+@echo.
+@echo off   
 @powershell rm $(spicetify -c) >nul
+spicetify backup aplly >nul
 echo y|spicetify restore backup >nul
 spicetify apply >nul
 spicetify config current_theme Dobby >nul
